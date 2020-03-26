aa7aba24488eed0fc28fc8d8e197f64596463c2d
Konstantin <dauer.konstantin@gmail.com>
2020-03-26 17:58:26 +0100

    run build

---
+++ build/precache-manifest.fad6b621d4d97c28b739d6f8205f17f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609e1cf0726bc83d89db7a6f79dc70ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "944a7612fd58cbca74df",
+    "url": "/static/css/main.a8fed5b6.chunk.css"
+  },
+  {
+    "revision": "620f2d10731fe5ed0220",
+    "url": "/static/js/2.e34c4e0b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e34c4e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944a7612fd58cbca74df",
+    "url": "/static/js/main.15492010.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
